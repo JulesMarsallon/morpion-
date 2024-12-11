ea03aefe273889bcdb3772ee2e999107a998c458
Jules MARSALLON-ALANORE <j.marsallonalanore@cite-scolaire.lan>
2024-12-11 13:12:07 +0100

    11/12/2024, séance pour commencer a voir la grille

--- Morpion/Program.cs
+++ Morpion/Program.cs
@@ -42,18 +42,18 @@ namespace Morpion
             int ColonneDébut = Console.CursorLeft; // par rapport au sommet de la fenêtre
 
             int essais = 0;    // compteur d'essais
-	        int joueur = 1 ;   // 1 pour la premier joueur, 2 pour le second
-	        int l, c = 0;      // numéro de ligne et de colonne
+            int joueur = 1;   // 1 pour la premier joueur, 2 pour le second
+            int l, c = 0;      // numéro de ligne et de colonne
             int j, k = 0;      // Parcourir le tableau en 2 dimensions
             bool gagner = false; // Permet de vérifier si un joueur à gagné 
             bool bonnePosition = false; // Permet de vérifier si la position souhaité est disponible
 
-	        //--- initialisation de la grille ---
+            //--- initialisation de la grille ---
             // On met chaque valeur du tableau à 10
-	        for (j=0; j < grille.GetLength(0); j++)
-		        for (k=0; k < grille.GetLength(1); k++)
-			        grille[j,k] = 10;
-            while(!gagner && essais != 9)
+            for (j = 0; j < grille.GetLength(0); j++)
+                for (k = 0; k < grille.GetLength(1); k++)
+                    grille[j, k] = 10;
+            while (!gagner && essais != 9)
             {
 
                 // A compléter 
@@ -63,7 +63,7 @@ namespace Morpion
                     Console.WriteLine("Colonne =    ");
                     // Peut changer en fonction de comment vous avez fait votre tableau.
                     Console.SetCursorPosition(LigneDébut + 10, ColonneDébut + 9); // Permet de manipuler le curseur dans la fenêtre 
-                    l = int.Parse(Console.ReadLine()) - 1; 
+                    l = int.Parse(Console.ReadLine()) - 1;
                     // Peut changer en fonction de comment vous avez fait votre tableau.
                     Console.SetCursorPosition(LigneDébut + 10, ColonneDébut + 10); // Permet de manipuler le curseur dans la fenêtre 
                     c = int.Parse(Console.ReadLine()) - 1;
@@ -85,6 +85,6 @@ namespace Morpion
             // A compléter 
 
             Console.ReadKey();
+        }
     }
-  }
 }
