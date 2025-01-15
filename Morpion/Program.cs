5950a2ee0885062522c74f57a17b6b357c05639d
Jules MARSALLON-ALANORE <jules.marsallon@gmail.com>
2025-01-15 13:15:08 +0100

    J'ai enfin réussie ma gestion d'erreur. Il me reste a finalisé les commentaire et la structure. Je vais terminer tout cela dans la séance du 15/01/2052. j'aurais donc finis le morpion a cette date

--- Morpion/Program.cs
+++ Morpion/Program.cs
@@ -5,7 +5,7 @@ using System;
 
 //i : sert a bouger dans le tableau , il permet de changer le nombre (1,2 ou " ") a chaque passager utilisateur , i c donc les ligne 
 //j : sert a bouger dans le tableau ,  il permet de changer le nombre (1,2 ou " ") a chaque passager utilisateur , c donc les colone
-namespace Morpion 
+namespace Morpion
 {
     class ProgrameMorpi // classe du jeux
     {
@@ -15,7 +15,7 @@ namespace Morpion
                                                      //int[,] : permet de dire que c'est un tableau 2d (matrice) et chaque élément c un int 
                                                      //[,] : la virgule c pr dire que c un tableau de tableau, chaque "petit" tableau vaut 0 nativement 
                                                      //new int[3, 3] : met la grille en 3 colone 3 ligne , chaque matrice sera un entier, dans le cas ici 0 nativement, 1 ou 2 ( pour les joueur)
-        //affiche grille Morpion 
+                                                     //affiche grille Morpion 
         public static void AfficherMorpion()
         {
             Console.Clear(); // Efface la console ,permet de réinitialisé la console 
@@ -46,7 +46,7 @@ namespace Morpion
                                            // si la case est 1 cela veut donc dire que c le joueur O qui a mis sont signe, si c 2 c le joueur X qui a mis
                                            //sont signe , et 0 ya rien 
 
-                    Console.Write(" O "); //c le joueur 1
+                        Console.Write(" O "); //c le joueur 1
                     else if (grille[i, j] == 2) // Si la case contient un 2, affiche "X" 
                                                 // Si la case contient un 1, affiche "O"
                                                 // grille[i, j] (vas a la ligne i et la colonne j )
@@ -57,8 +57,8 @@ namespace Morpion
                         Console.Write(" X "); // c le joueur 2
                     else // Sinon, laisse la case vide
                         Console.Write("   "); //et donc sa marque rien qui mis comme un espace ( qui vaut 0 de base) le rien vaut 0 donc le tableau de base est : 0 0 0 
-                                                                                                                                                                //0 0 0
-                                                                                                                                                                //0 0 0
+                                              //0 0 0
+                                              //0 0 0
                     if (j < 3) // barre verticale entre colonne 
                                // j (la colone au dessu)
                                // < 3 (vérif que j est avant la derniére colone qui sont elle de 0 a2, donc il vérif que l'on est soit dans la 0 soit dans la 2
@@ -86,7 +86,7 @@ namespace Morpion
         //joueur : c'est le joueur , 1 pour O , 2 pour X
 
         {
-            if (l < 0 || l > 2 || c < 0 || c > 2 || grille[l, c] != 0)  
+            if (l < 0 || l > 2 || c < 0 || c > 2 || grille[l, c] != 0)
             // ce if permet de vérifier tout les condition en meme temps, cette structure permet de faire un peut plus court et simple tout en ne modifiant pas le fonctionnement de base. 
             // Vérifie si la case est valide et vide                                                     
             // l < 0 : vérif si ligne choisit par le joueur et inférieur a 0 , si oui invalid car hors grille                                                       
@@ -199,48 +199,62 @@ namespace Morpion
 
             return false; // Retourne faux si aucun joueur n'a gagné
         }
+
+
         // Point d'entrée du programme principal
         static void Main(string[] args)
         //métode main ( par défault elle a ce nom, le code commence tjrs d'ici
         //string[] args : DEMANDER POUR CONPRENDRE, visual ma mis sa en correct
+
         {
-            int joueur = 1; // declare le joueur actuel (1 ou 2)
-            int essais = 0; // compte nombre coups jouer
-            bool gagner = false; // indique si joueur gagner
-            int l; 
-            int c; // cordonnees du coup
+            int joueur = 1; // Déclare le joueur actuel (1 ou 2)
+            int essais = 0; // Compte le nombre de coups joués
+            bool gagner = false; // Indique si un joueur a gagné
+            int l = -1;  // Initialisation de la variable ligne (-1 car tableau )
+            int c = -1;  // Initialisation de la variable colonne  (-1 car tableau )
 
-            for (int i = 0; i < 3; i++) // Initialise la grille avec une valeur 10 pour indiquer que toutes les cases sont vides
-                                        // Boucle, int i = 0 (initialise varibale i a 0),
-                                        // i < 3 (c la condition pour que la boucle continue, tant que i est plus petit que 3 la boucle continue )  c un compte tour si 3 symbole identique win.
-                                        // i++ ajouter 1 a i
-                                        //Pour résumer quand i atteint 3 la boucle se stop 
+            // Initialisation de la grille (toutes les cases sont vides)
+            for (int i = 0; i < 3; i++)
+            // Initialise la grille avec une valeur 10 pour indiquer que toutes les cases sont vides
+            // Boucle, int i = 0 (initialise varibale i a 0),
+            // i < 3 (c la condition pour que la boucle continue, tant que i est plus petit que 3 la boucle continue )  c un compte tour si 3 symbole identique win.
+            // i++ ajouter 1 a i
+            //Pour résumer quand i atteint 3 la boucle se stop 
+
+
+            //RAPELLE
+            // c une boucle dans une boucle, la boucle i se déplace dans les ligne du tableau et la boucle j se déplace dans les colone
+            //c un moyen de se "déplace" dans le tableau ( la grille du jeux ^)
+            //si on rajouter a ce code:  Console.WriteLine($"Case ({i}, {j})"); on aurais toute les sorti comme 0,0 ou 2,1 etc
+
+            {
                 for (int j = 0; j < 3; j++)
-                    // Boucle pour parcourir les colonnes
-                    // int j = 0 ( instaure j et le met a 0)
-                    // j < 3 ( tant que j est plus petit que 3 boucle continue )  c un compte tour si 3 symbole identique win.
-                    // j++ ( incrémentation de j de 1)
+                // Boucle pour parcourir les colonnes
+                // int j = 0 ( instaure j et le met a 0)
+                // j < 3 ( tant que j est plus petit que 3 boucle continue )  c un compte tour si 3 symbole identique win.
+                // j++ ( incrémentation de j de 1)
 
-                    //RAPELLE
-                    // c une boucle dans une boucle, la boucle i se déplace dans les ligne du tableau et la boucle j se déplace dans les colone
-                    //c un moyen de se "déplace" dans le tableau ( la grille du jeux ^)
-                    //si on rajouter a ce code:  Console.WriteLine($"Case ({i}, {j})"); on aurais toute les sorti comme 0,0 ou 2,1 etc
-                   
-                    grille[i, j] = 0; //met la valleur de la grille ( les case vide ) a 0, assigne une valeur dans les élément de la matrioce
-                                      //grille : la grille 3*3
-                                      //[i, j] :ligne et colonnes de matrice
-                                      //=10 on met 10 au coordonnée donné 
-                                      //=0 on met 0 au coordonnée donné 
-                                      //permet a la martrice d'avoir des info deja mise dans la grille , pour pouvoir l'initialisé 
 
-            while (!gagner && essais < 9) //sa continue tant que le nombre d'eesssaie n'est pas finit et que persone a win 
-                                          //&& permet de mettre 2 condition dans while , il peut marcher avec un if ou d'autre structure logique 
-                                          //while : c la boulce (tant que)
-                                          //!gagner varribale (win ou lose), ! c un opérateur qui sert a dire qu'un joueur win , si gagner et true alors il devien fals ( grace a !) ce qui permet a la boucle de stop (et inversement)
-                                          //< 9 , la boucle continue tant qu'on est pas a 9 tour
-    
+                {
+                    grille[i, j] = 0;//met la valleur de la grille ( les case vide ) a 0, assigne une valeur dans les élément de la matrioce
+                                     //grille : la grille 3*3
+                                     //[i, j] :ligne et colonnes de matrice
+                                     //=10 on met 10 au coordonnée donné 
+                                     //=0 on met 0 au coordonnée donné 
+                                     //permet a la martrice d'avoir des info deja mise dans la grille , pour pouvoir l'initialisé 
+
+                }
+            }
+
+            while (!gagner && essais < 9)//sa continue tant que le nombre d'eesssaie n'est pas finit et que persone a win 
+                                         //&& permet de mettre 2 condition dans while , il peut marcher avec un if ou d'autre structure logique 
+                                         //while : c la boulce (tant que)
+                                         //!gagner varribale (win ou lose), ! c un opérateur qui sert a dire qu'un joueur win , si gagner et true alors il devien fals ( grace a !) ce qui permet a la boucle de stop (et inversement)
+                                         //< 9 , la boucle continue tant qu'on est pas a 9 tour
+
             {
                 AfficherMorpion(); // Affiche la grille finale (donc sans win just les o et x) (avec les information en dessous)
+
 
                 Console.WriteLine($"C'est au tour du joueur {joueur}");  // indique de joueur 
                                                                          //$ : ce carctére permet de dire que {joueur} est une varriable qui peut etre modifier (dans cette situation c pr dire joueur 1 ou 2
@@ -250,107 +264,63 @@ namespace Morpion
 
 
 
-
-
-
-                /*
-                Console.Write("Colonne de 1 à 3 : ");
-                string input1 = Console.ReadLine(); //Console.. :métohde qui attend utilisateur rentre texte 
-                                                    //
-
-                if (int.TryParse(input1, out int temp1)) // int.TryParse : vérif si sa peut etre un entier 
-                                                       // si c bon , c stocké dans out int temp
-                                                       // donc si on met chiffre = true , si on met des lettre fals
+                // Demande la ligne
+                bool validLigne = false;
+                while (!validLigne)
                 {
-                    if (temp1 >= 1 && temp1 <= 3) //valeur temp est temporaire et permert de stocker une valeur
+                    Console.Write("Ligne de 1 à 3 : ");
+                    string inputL = Console.ReadLine();
+                    if (int.TryParse(inputL, out l) && l >= 1 && l <= 3) //& double condiution 
+                                                                         //Console.Write("Colonne de 1 à 3 : "); : affiche msg
+                                                                         //string inputC = Console.ReadLine(); : attend que l'utilisateur mette un touche 
+                                                                         //  if (int.TryParse(inputC, out c) && c >= 1 && c <= 3) // vérificateur
                     {
-                        c = temp1 - 1; //le -1 du tableau et tt ( 0.1.2)
+                        l--; // On ajuste la valeurs pour correspondrer à l'index de la grile (0-2)
+                        validLigne = true;
                     }
                     else
                     {
-                        Console.WriteLine("La colonne doit être entre 1 et 3.");
-                        // pas bon car doit etre en 1 et 3
+                        Console.WriteLine("pas bon, saisir une ligne entre 1 et 3.");
                     }
                 }
-                else
+
+                // Demande la colonne
+                bool validColonne = false;
+                while (!validColonne)
                 {
-                    Console.WriteLine("Entrée invalide. Veuillez saisir un nombre.");
-                    // pas bon car pas 1.2.3
-                }
-
-
-                
-                
-                Console.Write("Ligne de 1 à 3 : ");
-                string input2 = Console.ReadLine(); //Console.. :métohde qui attend utilisateur rentre texte 
-                                                    //
-
-                if (int.TryParse(input2, out int temp2)) // int.TryParse : vérif si sa peut etre un entier 
-                                                             // si c bon , c stocké dans out int temp
-                                                             // donc si on met chiffre = true , si on met des lettre fals
-                {
-                    if (temp2 >= 1 && temp2 <= 3) //valeur temp est temporaire et permert de stocker une valeur
+                    Console.Write("Colonne de 1 à 3 : ");
+                    string inputC = Console.ReadLine();
+                    if (int.TryParse(inputC, out c) && c >= 1 && c <= 3) //& double condiution 
+                    //Console.Write("Colonne de 1 à 3 : "); : affiche msg
+                    //string inputC = Console.ReadLine(); : attend que l'utilisateur mette un touche 
+                    //  if (int.TryParse(inputC, out c) && c >= 1 && c <= 3) // vérificateur 
                     {
-                        c = temp2 - 1; //le -1 du tableau et tt ( 0.1.2)
+                        c--; // On ajuste la valeur pour correspondre à l'index de la grille (0-2)
+                        validColonne = true;
                     }
                     else
                     {
-                        Console.WriteLine("La colonne doit être entre 1 et 3.");
-                        // pas bon car doit etre en 1 et 3
+                        Console.WriteLine("pas bon saisir une colonne entre 1 et 3.");
                     }
                 }
-                else
+
+                // Si le coup est valide, on le joue
+                if (AJouer(l, c, joueur))
                 {
-                    Console.WriteLine("Entrée invalide. Veuillez saisir un nombre.");
-                    // pas bon car pas 1.2.3
-                }
-               */
+                    essais++; // Incrémente le nombre d'essais
 
-
-
-               Console.Write("Ligne de 1 à 3 : "); // quelle ligne ? 
-                l = int.Parse(Console.ReadLine()) - 1; // prend le chiffre mis par l'utilisateur fait -1 car sinon sa décal tt, le tableau c pas 1.2.3 mais 0.1.2,
-               // try
-               // {
-               // l = int.Parse(Console.ReadLine()) - 1; // prend le chiffre mis par l'utilisateur fait -1 car sinon sa décal tt, le tableau c pas 1.2.3 mais 0.1.2,
-               // } 
-               // catch ((l >= 1 && l <= 3)   )
-
-
-
-
-
-
-
-
-
-                Console.Write("Colonne de 1 à 3 : "); // quelle colone ?
-                c = int.Parse(Console.ReadLine()) - 1; // prend le chiffre mis par l'utilisateur , fait -1 car sinon sa décal tt, le tableau c pas 1.2.3 mais 0.1.2
-
-
-
-
-
-                // Si le coup est valide, jouer
-                if (AJouer(c, l, joueur))
-                {
-                    essais++; // compte tour ( si  9 la parti de stop)
-
-
-                    if (Gagner(joueur)) // Si joueur gagne alors : 
+                    if (Gagner(joueur)) // Si le joueur a gagné
                     {
-                        AfficherMorpion(); // Affiche la grille finale 
-                        Console.WriteLine($"Le joueur {joueur} gaggne ");
-                        gagner = true; // Arrête le jeu car le joueur a gagner (condition gagner vérifier)
+                        AfficherMorpion(); // Affiche la grille finale
+                        Console.WriteLine($"Le joueur {joueur} gagne !");
+                        gagner = true; // Arrête le jeu car un joueur a gagné
                     }
-                    else if (essais == 9) // Sinombre essais atteint 9 sans gagnant
+                    else if (essais == 9) // Si on atteint 9 essais sans gagnant
                     {
-                        AfficherMorpion(); // Affiche la grille finale (donc sans win just les o et x)
-                        Console.WriteLine("Egalitè persone ne gagne");
+                        AfficherMorpion(); // Affiche la grille finale
+                        Console.WriteLine("Egalité, personne ne gagne.");
                         gagner = true; // Arrête le jeu
                     }
-
-
                     //joueur c la varriable qui est le joueur qui joue (la ya 2 joueur , 1(O) et 2(X) )
 
                     if (joueur == 1)  //c pr vérif si la on est joueur 1 passe au 2 et vis versa
@@ -362,26 +332,24 @@ namespace Morpion
                         joueur = 1; // sinon (la il est 2), on  change a 1
                     }
 
+
                 }
                 else
                 {
-                    Console.WriteLine("pas bon mouvement"); // Message d'erreur si le coup est invalide
-
-
+                    Console.WriteLine("pas bon mouvement , refaire :");
                 }
-
-
-
             }
-            Console.WriteLine("Fin"); // Message fin
-            //Console.ReadKey(); //touche random pr close
+
+            Console.WriteLine("Fin du jeu.");
+
 
             Console.WriteLine("            ");
             Console.WriteLine("            ");
-            Console.WriteLine("Apuyer sur une touche pour fermer");
+            Console.WriteLine("Appuyez sur une touche pour fermer.");
             Console.WriteLine("            ");
             Console.WriteLine("            ");
             Console.WriteLine("            ");
+            Console.ReadKey();
         }
         //Console.ReadKey();
         //fin prog principale 
